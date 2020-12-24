09ab3c373c84cc79323ffe0d5e3d8403feb2c291
Shish <shish@shishnet.org>
2020-12-24 18:50:01 +0000

    bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="eventtracer-py",
-    version="0.0.1",
+    version="0.0.2",
     author="Shish",
     author_email="webmaster@shishnet.org",
     description="A library for tracing code into the chrome Event Trace format",
